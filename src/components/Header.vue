045820e9125acb664a698f77fb481b175193955d
Corfaya <lauracorrado0696@gmail.com>
2024-08-29 16:30:20 +0200

    [UPD] banner moved to its own component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,17 +1,16 @@
 <script>
+import Banner from './partials/Banner.vue';
 export default {
-
+  components: {
+    Banner
+  }
 }
 </script>
 
 <template>
   <header>
     <!-- BANNER -->
-    <div class="banner text-center p-3 d-flex justify-content-center align-items-center">
-      <p>Starts <span class="text-uppercase">Tomorrow</span>! Our biggest event of the year...</p>
-      <p><i class="fa-regular fa-clock"></i> <span class="fw-bold timeout ms-1">00:00:00:00</span></p>
-      <button type="button" class="btn text-white ms-4 fw-bold">Get ticket</button>
-    </div>
+    <Banner />
     <!-- NAVBAR -->
     <div class="container">
       <nav class="navbar navbar-expand-lg py-3">
@@ -93,19 +92,19 @@ export default {
                 </ul>
               </li>
             </ul>
-      
+
           </div>
           <div class="d-none d-lg-flex" role="search">
-              <i class="fa-brands fa-twitter"></i>
-              <i class="fa-brands fa-facebook-f"></i>
-              <i class="fa-brands fa-instagram"></i>
-              <i class="fa-brands fa-linkedin"></i>
-            </div>
+            <i class="fa-brands fa-twitter"></i>
+            <i class="fa-brands fa-facebook-f"></i>
+            <i class="fa-brands fa-instagram"></i>
+            <i class="fa-brands fa-linkedin"></i>
+          </div>
         </div>
       </nav>
     </div>
 
-    
+
 
   </header>
 </template>
@@ -114,26 +113,6 @@ export default {
 @use "../style/partials/vars" as *;
 
 header {
-  .banner {
-    background-color: $grey;
-    font-size: 14px;
-
-    p {
-      margin: 0 10px;
-
-      .timeout {
-        letter-spacing: 3px;
-        font-size: 16px;
-      }
-    }
-
-    button {
-      background-color: $orange_main;
-      padding: 5px 40px;
-      font-size: 14px;
-    }
-  }
-
   nav {
     .navbar-brand {
       img {
---
+++ src/components/partials/Banner.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+    
+}
+</script>
+<template>
+    <div class="banner text-center p-3 d-flex justify-content-center align-items-center">
+        <p>Starts <span class="text-uppercase">Tomorrow</span>! Our biggest event of the year...</p>
+        <p><i class="fa-regular fa-clock"></i> <span class="fw-bold countdown ms-1">00:00:00:00</span></p>
+        <button type="button" class="btn text-white ms-4 fw-bold">Get ticket</button>
+      </div>
+</template>
+<style lang="scss">
+@use "../../style/partials/vars" as *;
+.banner {
+    background-color: $grey;
+    font-size: 14px;
+
+    p {
+      margin: 0 10px;
+
+      .countdown {
+        letter-spacing: 3px;
+        font-size: 16px;
+      }
+    }
+    button {
+      background-color: $orange_main;
+      padding: 5px 40px;
+      font-size: 14px;
+    }
+}
+</style>
