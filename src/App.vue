f343d8981de62267d18851d5063d90a81bce1a90
Corfaya <lauracorrado0696@gmail.com>
2024-08-29 12:27:50 +0200

    [UPD] scaffolding and components creation

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
-<script >
-
+<script>
+import Header from './components/Header.vue';
+import Main from './components/Main.vue';
+import Footer from './components/Footer.vue';
+export default {
+    components: {
+        Header,
+        Main,
+        Footer
+    }
+}
 </script>
 <template>
 
