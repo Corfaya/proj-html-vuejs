d5280e6219f90094c7996390bbae45225ce80610
Corfaya <lauracorrado0696@gmail.com>
2024-08-30 10:25:07 +0200

    [UPD] First division of the main

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -5,7 +5,23 @@ export default {
 </script>
 
 <template>
+<main class="my-3">
+    <!-- HERO -->
 
+    <!-- First section -->
+
+    <!-- Second Section -->
+
+    <!-- Video Section -->
+
+    <!-- Carousel -->
+
+    <!-- Images Grid -->
+
+    <!-- Imgs positions section -->
+
+    <!-- Images Grid 2 -->
+</main>
 </template>
 
 <style lang="scss" scoped>
