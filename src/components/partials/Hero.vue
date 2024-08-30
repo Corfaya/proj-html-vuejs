6d6b1c885bec99c5608af8056c95367618260543
Corfaya <lauracorrado0696@gmail.com>
2024-08-30 12:28:59 +0200

    [UPD] Hero section completed

--- src/components/partials/Hero.vue
+++ src/components/partials/Hero.vue
@@ -1,16 +1,20 @@
 <script>
 export default {
-    
+
 }
 </script>
 <template>
     <!-- BACKGROUND HERO -->
-    <div class="bg-hero">
+    <div class="bg-hero position-relative">
+        <!-- IMAGES -->
+        <div class="obj-fit first"><img src="../../assets/img/artist-course-05-480x480.jpg" alt=""></div>
+        <div class="obj-fit second"><img src="../../assets/img/artist-course-08-480x480.jpg" alt=""></div>
+        <div class="obj-fit third"><img src="../../assets/img/artist-hero-image-04.jpg" alt=""></div>
         <div class="container d-flex justify-content-center">
             <!-- TEXT -->
             <div class="d-flex flex-column align-items-center hero-text-box">
                 <h1 class="m-0">Hello, Im Matin</h1>
-                <h2  class="text-center">Artist Coaching And Mentoring Might Be for You</h2>
+                <h2 class="text-center">Artist Coaching And Mentoring Might Be for You</h2>
                 <button type="button" class="btn text-white fw-bold">
                     Get started Today
                 </button>
@@ -21,6 +25,7 @@ export default {
 </template>
 <style lang="scss" scoped>
 @use "../../style/partials/vars" as *;
+
 .bg-hero {
     background-color: $hero;
     padding: 200px 0;
@@ -28,30 +33,66 @@ export default {
     background-repeat: no-repeat;
     background-position: right, left top, left 500px bottom 10%, right 5% top 20%, left, right 80px bottom;
     background-size: contain, 900px 400px, 10%, 100px, 300px, 250px;
+    
+    .obj-fit {
+        position: absolute;
+        box-shadow: 0px 2px 20px 3px rgb(151, 150, 150);
+        &.first {
+            max-width: 200px;
+            height: 220px;
+            z-index: 2;
+            left: 150px;
+            bottom: 50px;
+            filter: contrast(110%)
+        }
+
+        &.second, &.third {
+            max-width: 250px;
+            height: 300px;
+            z-index: 1;
+        }
+
+        &.second {
+            top: 50px;
+            left: 320px;
+        }
+
+        &.third {
+            bottom: 50px;
+            right: 150px;
+        }
+    }
+    img{
+        width: 100%;
+        height: 100%;
+    }
+
     .hero-text-box {
         gap: 10px;
         max-width: 350px;
+
         h1 {
             font-family: $artistic_font;
             color: $sky;
             font-size: 22px;
         }
-    
+
         h2 {
             font-size: 22px;
             font-weight: 700;
             line-height: 30px;
         }
-    
+
         button {
             width: 200px;
             background-color: $orange_main;
             padding: 10px;
             font-size: 14px;
+
             &:hover {
-              background-color: $sky;
+                background-color: $sky;
             }
-          }
+        }
     }
-}    
+}
 </style>
