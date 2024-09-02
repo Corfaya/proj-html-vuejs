90c4a0a8823a844846f209a84f96708ead61c670
Corfaya <lauracorrado0696@gmail.com>
2024-09-02 11:56:59 +0200

    [UPD] Exercise hover required done

--- src/components/partials/OnlineCoursesList.vue
+++ src/components/partials/OnlineCoursesList.vue
@@ -7,17 +7,20 @@ export default {
 </script>
 <template>
     <div class="col-3">
-        <div class="box-course d-flex flex-column mb-4">
-            <img :src="`./src/assets/img/${online_class.src}`" :alt="online_class.alt">
-            <div class="mx-3 my-3">
-                <p class="text-orange fw-bold my-1"> {{ online_class.price }} </p>
-                <p class="fw-bold"> {{ online_class.title }} </p>
+        <div class="box-course d-flex flex-column mb-4 position-relative">
+            <div class="img-container">
+                <img :src="`./src/assets/img/${online_class.src}`" :alt="online_class.alt">
+            </div>
+            <!--  -->
+            <div class="text-container p-3">
+                <p class="text-orange fw-bold my-1 fs-18"> {{ online_class.price }} </p>
+                <p class="fw-bold title fs-20"> {{ online_class.title }} </p>
                 <span>
-                    <i class="fa-regular fa-file-lines me-1"></i>
+                    <i class="fa-regular fa-file-lines me-1 fs-14"></i>
                     {{ online_class.duration }} Lessons
                 </span>
                 <span class="ms-3">
-                    <i class="fa-regular fa-user me-1"></i>
+                    <i class="fa-regular fa-user me-1 fs-14"></i>
                     {{ online_class.students }} Students</span>
             </div>
         </div>
@@ -29,15 +32,57 @@ export default {
 
 .box-course {
     margin: 0 auto;
-    img {
-        cursor: pointer;
+    height: 530px;
+    overflow: hidden;
+
+    .img-container {
+        width: 100%;
+        height: 100%;
+        overflow: hidden;
+
+        img {
+            cursor: pointer;
+            width: 100%;
+            transition: transform 1s ease-in;
+
+            &:hover {
+                transform: scale(1.2)
+            }
+        }
     }
-    .text-orange {
-        color: $orange_main;
+
+    .text-container {
+        height: calc(100% - 320px);
+        max-width: calc(100% - 50px);
+        position:absolute;
+        bottom: 0;
+        left: 0;
+
+        transition: all 0.8s;
+        border: 2px solid transparent;
+        transform: translateY(0);
+        background-color: transparent;
+
+        .text-orange {
+            color: $orange_main;
+        }
+
+        .title:hover {
+            color: $orange_main;
+            cursor: pointer;
+        }
+        
+        span {
+            color: grey;
+            font-size: 10px;
+        }
     }
-    span {
-        color: grey;
-        font-size: 10px;
+
+    &:hover .text-container {
+        border: 2px solid $orange_main;
+        transform: translateY(-50px);
+        background-color: $white;
     }
 }
+
 </style>
